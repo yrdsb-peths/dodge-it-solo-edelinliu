bd8fcdf57f07c8b79c2c4afb30331839d03028fd
edelinliu <440009774@gapps.yrdsb.ca>
2026-04-16 08:35:50 -0400

    Add Hero

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,10 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
-    public MyWorld() {
+    public MyWorld() 
+    {
         super(600, 400, 1);
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
     }
 }
